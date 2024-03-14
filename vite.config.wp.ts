ffa1e536c54816ebbacc7f034852568eaf139427
niccolo <nicco.castelli@gmail.com>
2024-03-14 19:25:34 +0100

    - zip tema wordpress

--- vite.config.wp.ts
+++ vite.config.wp.ts
@@ -2,17 +2,59 @@ import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import fs from "fs";
+//import * as fs_extra from "fs-extra";
+import archiver from "archiver";
+
 
 import usePHP from "vite-plugin-php";
+
+async function zipDirectory(sourceDir: string, outPath: string): Promise<void> {
+  console.log("zipDirectory", sourceDir, outPath);
+  const output = fs.createWriteStream(outPath);
+  const archive = archiver("zip", { zlib: { level: 9 } }); // Compression level: 0 (fastest) - 9 (highest)
+  console.log("output", output, archive);
+
+  return new Promise((resolve, reject) => {
+    output.on("close", () => resolve());
+    output.on("end", () => console.log("Data has been drained"));
+
+    archive.on("warning", (err) => {
+      console.warn("Archive warning", err);
+      if (err && err?.code === "ENOENT") {
+        console.warn(err);
+      } else if (err) {
+        reject(err);
+      }
+    });
+
+    archive.on("error", (err) => {
+      console.error("Archive error", err);
+      if (err) {
+        console.error("Archive error:", err);
+        reject(err);
+      }
+    });
+
+    archive.pipe(output);
+    archive.directory(sourceDir, false);
+    archive.finalize();
+  });
+}
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 const bundleHooks: PluginOption = {
   closeBundle: () => {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
+      const themeDir = path.resolve(__dirname, "wp/theme");
+      const themeFile = path.resolve(__dirname, "wp/artpay_theme.zip");
+
       fs.rename(path.resolve(__dirname, "wp/theme/static/index.php"), path.resolve(__dirname, "wp/theme/index.php"), () => {
-        console.log("------------ closeBundle ----------------");
-        resolve();
+        zipDirectory(themeDir, themeFile).then(() => {
+          resolve();
+        }).catch(() => {
+          reject();
+        });
       });
     });
   }
