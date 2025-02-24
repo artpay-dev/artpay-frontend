1f070858b637bac270b946674b920bc5a788d57b
giacomosx <giacomo.bartoli@me.com>
2025-02-24 09:03:25 +0100

    mod navbar for sandbox users

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -140,7 +140,7 @@ const Navbar: React.FC<NavbarProps> = ({ onMenuToggle }) => {
   );
 
   const menuLinks = [
-    //{ label: "Gallerie", href: "/gallerie ", requireAuth: true },
+    { label: "Gallerie", href: "/gallerie ", requireAuth: true },
     //{ label: "Collezionisti", href: "/artpay-per-collezionisti", requireAuth: false },
     { label: "Chi siamo", href: "/chi-siamo", requireAuth: false },
     //{ label: "ArtMatch", href: "https://artpay.art/art-match" }
@@ -175,16 +175,21 @@ const Navbar: React.FC<NavbarProps> = ({ onMenuToggle }) => {
           <Box sx={{ ml: 3 }}>
             {menuLinks
               .filter((l) => auth.isAuthenticated || !l.requireAuth)
-              .map((link, i) => (
-                <Button
-                  key={`btn-link-${i}`}
-                  sx={{ px: 2 }}
-                  onClick={() => handleNavigate(link.href)}
-                  color="inherit"
-                  variant="text">
-                  {link.label}
-                </Button>
-              ))}
+              .map((link, i) => {
+
+                if (link.label === "Chi siamo" && auth.isAuthenticated) return
+
+                return (
+                  <Button
+                    key={`btn-link-${i}`}
+                    sx={{ px: 2 }}
+                    onClick={() => handleNavigate(link.href)}
+                    color="inherit"
+                    variant="text">
+                    {link.label}
+                  </Button>
+                )
+              })}
           </Box>
         )}
         {/*<TextField sx={{flexGrow:0, ml: 1}} variant="standard" InputProps={{startAdornment: <InputAdornment position="start"><Search/></InputAdornment>}}/>*/}
