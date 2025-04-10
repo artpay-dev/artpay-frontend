a9845b6a38310345fc88c6e7ed330c9f01e07db1
giacomosx <giacomo.bartoli@me.com>
2025-04-10 15:43:35 +0200

    remove cta from lg

--- src/features/landingforcampaign/components/anchor/Anchor.tsx
+++ src/features/landingforcampaign/components/anchor/Anchor.tsx
@@ -1,6 +1,6 @@
 const Anchor = ({title = "Compila il form per essere ricontattato" } : {title?: string}) => {
   return (
-    <a href={"/landing-campaign/#brevo-form"} className={'text-[#6576EE] text-2xl my-12 block font-medium hover:text-[#4950e2] transition-all'}>
+    <a href={"/landing-campaign/#brevo-form"} className={'text-[#6576EE] text-2xl my-12 block font-medium hover:text-[#4950e2] transition-all lg:hidden'}>
       {title}
     </a>
   );
