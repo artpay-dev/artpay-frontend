a18c639e1ad48786b95d1c00a99097c7f73a29bc
giacomosx <giacomo.bartoli@me.com>
2025-10-01 10:09:37 +0200

    modo logo thothers

--- src/features/landingforcampaign/index.tsx
+++ src/features/landingforcampaign/index.tsx
@@ -12,7 +12,13 @@ const LandingForCampaign = () => {
         <div className="max-w-xl mb-12">
           <div className={"flex gap-2 items-center mb-6"}>
             <Logo className={"w-20"}/> {"x"}
-            <img src="/images/logo-TheOthers_W-300x134.png" alt="Logo the others logo" className={"w-24 top-2 relative invert"} />
+
+            <div
+              className={"w-20 h-10 top-2 relative invert bg-no-repeat bg-contain bg-center"}
+              style={{ backgroundImage: "url('/images/logo-TheOthers_W-300x134.png')" }}
+              role="img"
+              aria-label="Logo the others logo"
+            />
           </div>
           <p className={"text-lg leading-[125%] text-balance"}>
             The Others 2025 - XIV edizione sceglie artpay per promuovere l’utilizzo di soluzioni di pagamento digitali, flessibili e innovative al servizio di gallerie, artisti e collezionisti
