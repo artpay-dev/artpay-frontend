0712596228b8f6346db0366d1a65f0bbe654c38b
giacomosx <giacomo.bartoli@me.com>
2026-01-13 11:22:45 +0100

    fix artmatch card width

--- src/features/artmatch/components/feed-card/feed-card.tsx
+++ src/features/artmatch/components/feed-card/feed-card.tsx
@@ -6,7 +6,7 @@ const FeedCard = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="feed-card bg-[#1B2738] p-8 flex justify-between rounded-2xl h-full w-full max-w-xs md:max-w-4xl overflow-hidden">
+    <div className="feed-card bg-[#1B2738] p-8 flex justify-between rounded-2xl h-full w-full max-w-xs md:max-w-none overflow-hidden">
       <div className="flex flex-col justify-between">
         <div className={'flex flex-col gap-6 '}>
           <Typography variant="display3" color={'white'}>Voglia di match?</Typography>
